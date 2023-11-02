480979aedc839996ee7f12b8e99a4311f232f910
Mero <mero@meros-air.home>
2023-11-02 08:58:36 -0230

    add redux to the app

---
+++ src/redux/reducers.js
@@ -0,0 +1,9 @@
+// src/reducers/index.js
+import { combineReducers } from "redux";
+// Import your reducers here
+
+const rootReducer = combineReducers({
+  // Combine your reducers here
+});
+
+export default rootReducer;
